2e3fd2d1c4c2702949caafc1833882bb662f3f6c
Timm Nygren <metroid3prime@yahoo.com>
2015-10-31 02:33:21 -0600

    renames server project and added a client project

---
+++ BasicClientServer/Client/Client/client.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Client
+//
+//  Created by Timm Nygren on 10/31/15.
+//  Copyright © 2015 Timm Nygren. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
